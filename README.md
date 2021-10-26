b8a0db801a0c6b4308ebb2b1e2f2ee02692d62bf
adreider <adreidereconomic@gmail.com>
2021-10-26 11:03:20 -0300

    finalized

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
-# FinAPI - Financeira
+# FinAPI - Financeira 💵
+
+## Tecnologias 🚀
+ - Nodejs
+ - Express
+ - uuid
 
 ### Requisitos
 
 - [x] Deve ser possível criar uma conta
 - [x] Deve ser possível buscar o extrato bancário do cliente
-- [] Deve ser possível realizar um depósito
-- [] Deve ser possível realizar um saque
-- [] Deve ser possível buscar o extrato bancário do cliente po data
-- [] Deve ser possível atualizar dados da conta do cliente
-- [] Deve ser possível obter dados do cliente
-- [] Deve ser possível deletar uma conta
+- [x] Deve ser possível realizar um depósito
+- [x] Deve ser possível realizar um saque
+- [x] Deve ser possível buscar o extrato bancário do cliente po data
+- [x] Deve ser possível atualizar dados da conta do cliente
+- [x] Deve ser possível obter dados do cliente
+- [x] Deve ser possível deletar uma conta
+- [x] Deve ser possível retornar o balance
 
 ---
 
@@ -17,7 +23,7 @@
 
 - [x] Não deve ser possível cadastrar uma conta com CPF já existente
 - [x] Não deve ser possível fazer depósito em uma conta não existente
-- [] Não deve ser possível buscar extrato em uma conta não existente
-- [] Não deve ser possível fazer saque em uma conta não existente
-- [] Não deve ser possível excluir uma conta não existente
-- [] Não deve ser possível fazer saque quando o saldo for insuficiente
+- [x] Não deve ser possível fazer saque em uma conta não existente
+- [x] Não deve ser possível fazer saque quando o saldo for insuficiente
+- [x] Não deve ser possível buscar extrato em uma conta não existente
+- [x] Não deve ser possível excluir uma conta não existente
--- src/index.js
+++ src/index.js
@@ -16,10 +16,22 @@ function verifyIfExistsAccountCPF(request, response, next) {
   if (!customer) {
     return response.status(404).json({ error: 'Customer not found' });
   }
-  
+
   request.customer = customer;
 
   return next();
+};
+
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === "credit") {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
 };
 
 app.post("/account", (request, response) => {
@@ -46,7 +58,7 @@ app.post("/account", (request, response) => {
 
 app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
 
-  const { customer } =  request;
+  const { customer } = request;
 
   return response.json(customer.statement)
 });
@@ -67,6 +79,85 @@ app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
   customer.statement.push(statementOperation);
 
   return response.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+
+  const { amount } = request.body;
+
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({ error: "Insufficient funds!" })
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit",
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+
+});
+
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+
+  const { customer } = request;
+
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter(
+    (statement) => statement.created_at.toDateString() === new Date(dateFormat).toDateString()
+    
+  );
+
+  return response.json(statement);
+
+});
+
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+
+  const { name } = request.body;
+
+  const { customer } = request;
+  
+  customer.name = name;
+
+  return response.status(201).send();
+});
+
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+
+  const { customer } = request;
+
+  return response.json(customer);
+});
+
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+
+  const { customer } = request;
+
+  // splice
+  customers.splice(customer, 1);
+
+  return response.status(200).send(customers);
+
+});
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  return response.json(balance);
 });
 
 app.listen(3333, () => {
