223cd2349950d7b7da1bf00e9206cf9270398c36
adreider <adreidereconomic@gmail.com>
2021-10-26 09:43:33 -0300

    first commmit

---
+++ src/index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(404).json({ error: 'Customer not found' });
+  }
+  
+  request.customer = customer;
+
+  return next();
+};
+
+app.post("/account", (request, response) => {
+
+  const { cpf, name } = request.body;
+
+  const customersAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customersAlreadyExists) {
+    return response.status(400).json({ error: "customer already exists!" });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
+
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+
+  const { customer } =  request;
+
+  return response.json(customer.statement)
+});
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+
+  const { description, amount } = request.body;
+
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit",
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+app.listen(3333, () => {
+  console.log('listening on 3333 🚀');
+});
